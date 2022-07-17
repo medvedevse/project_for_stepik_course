2684f4f448de28ed7f9f78a93cf1474de03ec2a7
Stive <stepan.medvedev1995@yandex.ru>
2022-07-17 14:47:16 +0400

    Commit the page object test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,9 @@
 
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, link):
         self.browser = browser
-        self.url = url
+        self.link = link
 
     def open(self):
-        self.browser.get(self.url)
+        self.browser.get(self.link)
